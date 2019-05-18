85df097ef62e4e47c42ecc407131ed35d31d1d05
SunnySui <794086250@qq.com>
2019-05-18 16:12:50 +0800

    Add project and get ready for algorithm problems

---
+++ AlgorithmCollectionStudy/AlgorithmCollectionStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlgorithmCollectionStudy
+//
+//  Created by SunnySui on 2019/5/18.
+//  Copyright © 2019 IOS_Sunny_Sui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
