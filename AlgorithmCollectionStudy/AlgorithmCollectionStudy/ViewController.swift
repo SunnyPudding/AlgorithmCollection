c2786b5473fafd0bbbd7ebd3d55ee00f5aef9dd4
SunnySui <794086250@qq.com>
2019-06-03 17:20:36 +0800

    circular queue

--- AlgorithmCollectionStudy/AlgorithmCollectionStudy/ViewController.swift
+++ AlgorithmCollectionStudy/AlgorithmCollectionStudy/ViewController.swift
@@ -13,8 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var array = ["a","c","b","d"];
+        print(array);
+        swap(array: &array, p: 1, q: 2);
+        print(array);
+    }
+    //数组中的两个值进行交换
+    func swap<T> (array:inout[T],p:Int,q:Int){
+//        let temp = array[p];
+//        chars[p] = array[q];
+//        chars[q] = temp;
+        assert(p >= 0 && p < array.count)
+        assert(q >= 0 && q < array.count)
+       (array[p],array[q]) = (array[q],array[p])
     }
 
+}
+//用数组实现栈
+class Stack {
+    var stack:[AnyObject]
+    var isEmpty:Bool {return stack.isEmpty}
+    var peek:AnyObject?{return stack.last}
+    init() {
+      stack = [AnyObject]()
+    }
+    func  push (object:AnyObject) {
+        stack.append(object)
+    }
+    func pop() -> AnyObject? {
+        guard isEmpty  else {
+            return stack.removeLast()
+        }
+        return nil;
+    }
 
 }
 
